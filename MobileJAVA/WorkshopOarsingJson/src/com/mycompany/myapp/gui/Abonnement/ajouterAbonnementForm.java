3c54f18e9efb76ecf2ee041af89b1f8ab44b8473
as1302 <79095110+as1302@users.noreply.github.com>
2021-05-17 13:24:27 +0100

    Firas

--- MobileJAVA/WorkshopOarsingJson/src/com/mycompany/myapp/gui/Abonnement/ajouterAbonnementForm.java
+++ MobileJAVA/WorkshopOarsingJson/src/com/mycompany/myapp/gui/Abonnement/ajouterAbonnementForm.java
@@ -13,10 +13,12 @@ import com.codename1.ui.Command;
 import com.codename1.ui.Dialog;
 import com.codename1.ui.FontImage;
 import com.codename1.ui.Form;
+import com.codename1.ui.Label;
 import com.codename1.ui.TextField;
 import com.codename1.ui.events.ActionEvent;
 import com.codename1.ui.events.ActionListener;
 import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.spinner.NumericSpinner;
 
 /**
  *
@@ -32,6 +34,12 @@ public class ajouterAbonnementForm extends Form {
 
         TextField tfTitre = new TextField("", "Titre");
         TextField tfType = new TextField("", "Type");
+        Label prix = new Label("Prix");
+        NumericSpinner n1 = new NumericSpinner();
+        n1.setMin(60);
+        n1.setMax(220);
+        n1.setStep(40);
+        
         TextField tfPrix = new TextField("", "Prix");
         TextField tfDes = new TextField("", "Description");
         Button btnValider = new Button("Ajouter");
@@ -43,7 +51,9 @@ public class ajouterAbonnementForm extends Form {
                     Dialog.show("Alert", "Remplir Tous Les Champs SVP !", new Command("OK"));
                 } else {
                     try {
-                        Abonnement a = new Abonnement(tfTitre.getText(), tfType.getText(), Float.parseFloat(tfPrix.getText()), tfDes.getText());
+                        //Abonnement a = new Abonnement(tfTitre.getText(), tfType.getText(), Float.parseFloat(), tfDes.getText());
+                        Double d =n1.getValue();
+                        Abonnement a = new Abonnement(tfTitre.getText(), tfType.getText(),d.floatValue() , tfDes.getText());
                         if (ServiceAbonnement.getInstance().addAbonnement(a)) {
                             Dialog.show("Success", "Votre abonnement a été ajouter avec succès !", new Command("OK"));
                             previous.showBack();
@@ -59,7 +69,7 @@ public class ajouterAbonnementForm extends Form {
             }
         });
 
-        addAll(tfTitre, tfType, tfPrix, tfDes, btnValider);
+        addAll(tfTitre, n1,tfType, tfPrix, tfDes, btnValider);
         getToolbar().addMaterialCommandToLeftBar("", FontImage.MATERIAL_ARROW_BACK,
                  e -> previous.showBack()); // Revenir vers l'interface précédente
         
